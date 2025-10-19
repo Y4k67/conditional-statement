3a287f8b051a0d1f5a2edeadcfc4f69f97d2c9b8
Y4k67 <krishdevravi@gmail.com>
2025-10-19 14:00:09 +0300

    Add files via upload

---
+++ file4.py
@@ -0,0 +1,8 @@
+i=int(input("please enter a temperature"))
+if i<26:
+    print("i is smaller than 26")
+    print("i is a cold climate")
+else:
+    print("i is greater than 26")
+    print("i is a hot climate")
+    
